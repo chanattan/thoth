42a04aa5daa3bbb4dd2a133688f45e14f7507a35
Aloïs Rautureau <thinkingrocks@proton.me>
2025-10-02 17:22:06 +0200

    Curves with FBM

--- simulator/Curve.java
+++ simulator/Curve.java
@@ -2,31 +2,29 @@ import java.util.Random;
 public class Curve {
     float[] values;
 
-    public Curve(int steps, int news_freq_interval_min, int news_freq_interval_max, float chaos_factor, News[] news) {
+    public Curve(int steps, float fbm_min_movement, float fbm_max_movement, float chaos_factor, float[] event_effects) {
         Random rng = new Random();
         float[] values = new float[steps];
-        int next_news_counter = randomIntBounds(news_freq_interval_min, news_freq_interval_max, rng);
-        float last_news_effect = 0;
+        float[] fbm = new float[steps];
 
         for (int t = 0; t < steps; t += 1) {
-            float value = t == 0 ? 0 : values[t - 1];
-            if (next_news_counter == 0) {
-                int event_index = randomIntBounds(0, news.length, rng);
-                News event = news[event_index];
-                last_news_effect = event.getEffect();
-                value += event.getEffect();
-            } else {
-                value += last_news_effect * 0.75;
-                next_news_counter -= 1;
-            }
-
+            fbm[t] = rng.nextFloat(fbm_min_movement, fbm_max_movement) + (t == 0 ? 0 : fbm[t - 1]);
+            float value = event_effects[t] + (t == 0 ? 0 : values[t - 1]);
             values[t] = value;
+        }
+
+        for (int t = 0; t < steps; t += 1) {
+            values[t] = lerp(values[t], fbm[t], chaos_factor);
         }
 
         this.values = values;
     }
 
-    int randomIntBounds(int min, int max, Random rng) {
-        return rng.nextInt(max - min + 1) + min;
+    public float get(int t) {
+        return this.values[t];
+    }
+
+    float lerp(float a, float b, float factor) {
+        return a * factor + b * (1f - factor);
     }
 }
