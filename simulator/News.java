1a431accb47c33f9ccb36ee805597a0301478902
chanattan <sok.chanattan@gmail.com>
2025-10-02 15:52:56 +0200

    second structure

---
+++ simulator/News.java
@@ -0,0 +1,5 @@
+public class News {
+    public static void test() {
+        System.out.println("appel de test");
+    }
+}
---
+++ simulator/Window.java
@@ -0,0 +1,6 @@
+public class Window {
+    public static void main(String[] args) {
+        News.test();
+    }
+
+}
