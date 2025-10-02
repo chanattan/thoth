0bb217ce2d755b68da5283b2a1cb818105a8f62d
chanattan <sok.chanattan@gmail.com>
2025-10-02 16:24:45 +0200

    first window

--- simulator/Window.java
+++ simulator/Window.java
@@ -1,6 +1,33 @@
-public class Window {
-    public static void main(String[] args) {
-        News.test();
-    }
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Window extends JFrame {
+	public Window() {
+		setSize(new Dimension(1280, 720));
+		getContentPane().setBackground(new Color(54, 54, 54));
+		setBackground(new Color(54, 54, 54));
+		setAlwaysOnTop(true);
+		getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.X_AXIS));
+		
+		JButton btnNewButton = new JButton("Invest");
+		btnNewButton.addActionListener((ActionEvent e) -> {
+            System.out.println("Investing...");
+                });
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(0, 143, 224));
+		getContentPane().add(panel);
+		getContentPane().add(btnNewButton);
+        setVisible(true);
+	}
+
+	private static final long serialVersionUID = 1L;
 
 }
+	
