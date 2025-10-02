f285aefa650a8fe41f35ad1ebdaa37e363e483dc
chanattan <sok.chanattan@gmail.com>
2025-10-02 17:36:09 +0200

    working on curve

--- logic/Curve.java
+++ logic/Curve.java
@@ -1,30 +1,35 @@
 import java.util.Random;
 public class Curve {
-    float[] values;
+    private float[] values;
+    private final int steps;
 
     public Curve(int steps, float fbm_min_movement, float fbm_max_movement, float chaos_factor, float[] event_effects) {
         Random rng = new Random();
-        float[] values = new float[steps];
+        this.values = new float[steps];
         float[] fbm = new float[steps];
 
         for (int t = 0; t < steps; t += 1) {
             fbm[t] = rng.nextFloat(fbm_min_movement, fbm_max_movement) + (t == 0 ? 0 : fbm[t - 1]);
-            float value = event_effects[t] + (t == 0 ? 0 : values[t - 1]);
-            values[t] = value;
+            float value = event_effects[t] + (t == 0 ? 0 : this.values[t - 1]);
+            this.values[t] = value;
         }
 
         for (int t = 0; t < steps; t += 1) {
-            values[t] = lerp(values[t], fbm[t], chaos_factor);
+            this.values[t] = lerp(this.values[t], fbm[t], chaos_factor);
         }
 
-        this.values = values;
+        this.steps = steps;
     }
 
-    public float get(int t) {
+    public float getSteps() {
+        return this.steps;
+    }
+
+    public float getValue(int t) {
         return this.values[t];
     }
 
-    float lerp(float a, float b, float factor) {
+    private float lerp(float a, float b, float factor) {
         return a * factor + b * (1f - factor);
     }
 }
---
+++ logic/Fund.java
@@ -0,0 +1,17 @@
+public class Fund {
+    private final String fundName;
+    private final Curve curve;
+
+    public Fund(String fundName, Curve curve) {
+        this.fundName = fundName;
+        this.curve = curve;
+    }
+
+    public String getFundName() {
+        return this.fundName;
+    }
+
+    public Curve getCurve() {
+        return this.curve;
+    }
+}
