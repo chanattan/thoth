4c6722e8962933f8b0fb7f66972ef11130e14da9
chanattan <sok.chanattan@gmail.com>
2025-10-09 18:18:52 +0200

    added basic invest action for the player, along TODOs.

--- thoth/logic/Player.java
+++ thoth/logic/Player.java
@@ -1,12 +1,16 @@
 package thoth.logic;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
 
 public class Player {
     private double capital;
+    private HashMap<Fund, ArrayList<Action>> actions;
 
     public Player(double capital) {
         this.capital = capital;
+        this.actions = new HashMap<Fund, ArrayList<Action>>();
     }
 
     public Player() {
@@ -18,7 +22,37 @@ public class Player {
         return this.capital;
     }
 
-    public void invest(Action a) {
+    /*
+        The player decides to invest in a given fund, at a given time (i.e., buys an action).
+        If the player has enough capital, the action is bought and stored among the actions the player has.
+        The value of an action can change over time.
+    */
+    public void invest(Action a) throws InsufficientCapital {
+        if (a.getValue() > this.capital)
+            throw new InsufficientCapital(a);
+        this.capital -= a.getValue();
+        this.actions.getOrDefault(a.getFund(), new ArrayList<Action>()).add(a);
+    }
+
+    public void sellAction(Action a) {
+        // TODO.
+        // updateCapital();
+    }
+
+    private static class InsufficientCapital extends Exception {
+
+        private String fundName;
+        private double value;
+
+        public InsufficientCapital(Action a) {
+            this.fundName = a.getFund().getName();
+            this.value = a.getValue();
+        }
+
+        @Override
+        public String getMessage() {
+            return "Player has insufficient funds for the fund " + this.fundName + ", at the current price of " + this.value + "€.";
+        }
         
     }
 }
