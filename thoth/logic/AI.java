46ee90a1d505e089c1c6725de5ce0349480c0587
chanattan <sok.chanattan@gmail.com>
2026-01-03 20:20:19 +0100

    confidence level, prediction, popup

--- thoth/logic/AI.java
+++ thoth/logic/AI.java
@@ -9,11 +9,33 @@ import thoth.simulator.Thoth;
 
 public class AI {
 
+    protected class Prediction {
+        public Fund fund;
+        public float confidence;
+        
+        public Prediction(Fund fund, float confidence) {
+            this.fund = fund;
+            this.confidence = confidence;
+        }
+
+        public float getExpectedReturn() {
+            return computeExpectedReturn(fund);
+        }
+
+        public int getConfidenceLevel() {
+            if (confidence > 0.75f) return 3; // High confidence
+            else if (confidence > 0.5f) return 2; // Medium confidence
+            else if (confidence > 0.25f) return 1; // Low confidence
+            else return 0; // No confidence
+        }
+
+    }
+
     private Thoth thoth;
     public AI(Thoth thoth) {
         this.thoth = thoth;
     }
-    public Fund predictNextMove() {
+    public Prediction predictNextMove() {
         Fund bestFund = null;
         int bestIncrease = Integer.MIN_VALUE;
         for (Fund f : thoth.funds) {
@@ -34,19 +56,63 @@ public class AI {
                 }
             }
         }
-        return bestFund; // Best action, may be null
+
+        if (bestFund != null) {
+            float confidence = computeConfidence(bestFund);
+            return new Prediction(bestFund, confidence);
+        }
+
+        return new Prediction(bestFund, 0.0f); // Best action, may be null
+    }
+
+    /*
+        * TODO: les curves étant prégénérables, on peut afficher en décalé antérieur les courbes et calculer un score de confiance
+        * en modèle boîte blanche sur les futures performances.
+        * 
+        * Computes a confidence score for the fund's recent performance: number of increases over total observations.
+        * Returns a float between 0 and 1.
+    */
+    public float computeConfidence(Fund fund) {
+        Curve c = fund.getCurve();
+        int[] lastValues = c.getLastValues(Math.max(0, c.getSteps() - 10));
+        int increases = 0;
+        for (int i = 1; i < lastValues.length; i++) {
+            if (lastValues[i] > lastValues[i - 1]) {
+                increases++;
+            }
+        }
+        return (float) increases / (lastValues.length - 1); // Proportion of increases
+    }
+
+    /**
+     * Computes the expected return for a given fund based on its recent performance: it is the percentage increase for the next time step.
+     * Returns a double representing the expected return percentage.
+     */
+    public float computeExpectedReturn(Fund fund) {
+        Curve c = fund.getCurve();
+        int[] lastValues = c.getLastValues(Math.max(0, c.getSteps() - 5));
+        if (lastValues.length < 2) return 0.0f;
+        return (float)(lastValues[lastValues.length - 1] - lastValues[0]) / lastValues[0]; // Percentage increase
     }
 
     public JPopupMenu popInfo(JComponent component, int x, int y) {
         JPopupMenu popup = new JPopupMenu();
 
-        Fund bestFund = predictNextMove();
-        if (bestFund != null) {
-            String fundName = bestFund.getName();
-            JLabel fundLabel = new JLabel("Thoth: Best Fund to Invest: " + fundName);
+        Prediction prediction = predictNextMove();
+        if (prediction.fund != null) {
+            String fundName = prediction.fund.getName();
+            JLabel fundLabel = new JLabel("<html><h4 align='center'>Thoth</h4><p style='color:orange'>> Best Fund to Invest:</p> <b>" + fundName + "</b><br>" +
+                " | Expected Increase: " + prediction.getExpectedReturn() + "<br>" +
+                " | Confidence Level: " + prediction.getConfidenceLevel() + "<br>" +
+                " <br> " +
+                " Note: This prediction is based on the last five months of the fund's performance.<br>" +
+                " <i>(Click anywhere to close this popup)</i></html>");
             popup.add(fundLabel);
         } else {
-            JLabel noFundLabel = new JLabel("Thoth: No clear best fund to invest.");
+            JLabel noFundLabel = new JLabel("<html><h4 align='center'>Thoth</h4><p style='color:red'>> No clear best fund to invest.</p><br>" +
+                " | Confidence Level: 0<br>"
+                + "<br>Note: Thoth is unable to determine with decent confidence which<br>fund is best to invest in at this time.<br>" +
+                "<i>(Click anywhere to close this popup)</i></html>");
             popup.add(noFundLabel);
         }
         
